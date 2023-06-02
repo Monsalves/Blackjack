69401d3a009c093607f5984ab9adb127037114e5
Monsalves <f.monsalves03@ufromail.cl>
2023-06-01 20:51:49 -0400

    2do commit

--- src/main/java/Baraja.java
+++ src/main/java/Baraja.java
@@ -13,35 +13,84 @@ public class Baraja {
         llenarCorazones();
         llenarDiamantes();
         llenarTreboles();
-
-        System.out.println(mazo.size());
     }
     public void mezclarMazo(){
         Collections.shuffle(mazo);
     }
     public void llenarPicas() {
         for (Numero num : Numero.values()) {
-            Carta cartita = new Carta(Pinta.PICAS, Color.NEGRO, num, 1);
+            int valorJuego = obtenerValorJuego(num);
+            Carta cartita = new Carta(Pinta.PICAS, Color.NEGRO, num, valorJuego);
             mazo.add(cartita);
         }
     }
     public void llenarTreboles() {
         for (Numero num : Numero.values()) {
-            Carta cartita = new Carta(Pinta.TRÉBOLES, Color.NEGRO, num, 1);
+            int valorJuego = obtenerValorJuego(num);
+            Carta cartita = new Carta(Pinta.TRÉBOLES, Color.NEGRO, num,valorJuego);
             mazo.add(cartita);
         }
     }
     public void llenarCorazones() {
         for (Numero num : Numero.values()) {
-            Carta cartita = new Carta(Pinta.CORAZONES, Color.ROJO, num, 1);
+            int valorJuego = obtenerValorJuego(num);
+            Carta cartita = new Carta(Pinta.CORAZONES, Color.ROJO, num, valorJuego);
             mazo.add(cartita);
         }
     }
     public void llenarDiamantes() {
         for (Numero num : Numero.values()) {
-            Carta cartita = new Carta(Pinta.DIAMANTES, Color.ROJO, num, 1);
+            int valorJuego = obtenerValorJuego(num);
+            Carta cartita = new Carta(Pinta.DIAMANTES, Color.ROJO, num, valorJuego);
             mazo.add(cartita);
         }
+    }
+    public int obtenerValorJuego(Numero num){
+        int valorJuego =0;
+        switch (num) {
+            case AS:
+                valorJuego = 11;
+                break;
+            case DOS:
+                valorJuego = 2;
+                break;
+            case TRES:
+                valorJuego = 3;
+                break;
+            case CUATRO:
+                valorJuego = 4;
+                break;
+            case CINCO:
+                valorJuego = 5;
+                break;
+            case SEIS:
+                valorJuego = 6;
+                break;
+            case SIETE:
+                valorJuego = 7;
+                break;
+            case OCHO:
+                valorJuego = 8;
+                break;
+            case NUEVE:
+                valorJuego = 9;
+                break;
+            case DIEZ:
+                valorJuego = 10;
+                break;
+            case JOTA:
+                valorJuego = 10;
+                break;
+            case QUINA:
+                valorJuego = 10;
+                break;
+            case KAISER:
+                valorJuego = 10;
+                break;
+        }
+        return valorJuego;
+
+
     }
     public ArrayList<Carta> getMazo() {
         return mazo;
--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -1,31 +1,31 @@
 import java.util.ArrayList;
-
-
 public class Jugador {
     private String nombre;
     private int puntaje = 0;
     ArrayList<Carta> mano = new ArrayList<Carta>();
 
+    //Constructor
     public Jugador(String nombre) {
         this.nombre = nombre;
     }
-    public String toString() {
-        return "Señor " +nombre+" tu mano es \n"+mano.get(0) +"\n" + mano.get(1) +"\n" + mano.get(2);
-    }
+
+
+
     public void agregarCarta(Carta cartita){
         mano.add(cartita);
-    }
-
-    public ArrayList<Carta> getMano() {
-        return mano;
-    }
-    public void setPuntaje(int puntaje){
-        this.puntaje = puntaje;
-
     }
     public int getPuntaje(){
         return puntaje;
     }
+    public void setPuntaje(int puntaje){
+        this.puntaje = puntaje;
+    }
     public String getNombre(){return nombre;}
+    public ArrayList<Carta> getMano() {
+        return mano;
+    }
+    public String toString() {
+        return "Señor " +nombre+" tu mano es \n"+mano.get(0) +"\n" + mano.get(1) +"\n" + mano.get(2);
+    }
 }
 
